bd2f1f3b442bf74667298d4c885b185b97e9b299
olesgedz <gedzoles@gmail.com>
2021-12-04 00:34:53 +0300

    d

--- src/include/ppu.h
+++ src/include/ppu.h
@@ -3,7 +3,7 @@
 #include <common.h>
 
 static const int LINES_PER_FRAME = 154;
-static const int TICKS_PER_LINE = 456;
+static const unsigned int TICKS_PER_LINE = 456; // unsigned
 static const int YRES = 144;
 static const int XRES = 160;
 
@@ -16,26 +16,26 @@ typedef enum {
 } fetch_state;
 
 typedef struct _fifo_entry {
-    struct _fifo_entry *next;
-    u32 value; //32 bit color value.
+    struct  _fifo_entry *next;
+    u32 value; // 32 bit color value
 } fifo_entry;
 
 typedef struct {
     fifo_entry *head;
     fifo_entry *tail;
     u32 size;
-} fifo;
+} fifo; // first in first out
 
-typedef struct {
-    fetch_state cur_fetch_state;
+typedef  struct {
+    fetch_state  cur_fetch_state;
     fifo pixel_fifo;
     u8 line_x;
     u8 pushed_x;
     u8 fetch_x;
     u8 bgw_fetch_data[3];
-    u8 fetch_entry_data[6]; //oam data..
-    u8 map_y;
+    u8 fetch_entry_data[6]; //aom data
     u8 map_x;
+    u8 map_y;
     u8 tile_y;
     u8 fifo_x;
 } pixel_fifo_context;
@@ -44,7 +44,6 @@ typedef struct {
     u8 y;
     u8 x;
     u8 tile;
-    
     unsigned f_cgb_pn : 3;
     unsigned f_cgb_vram_bank : 1;
     unsigned f_pn : 1;
@@ -54,6 +53,23 @@ typedef struct {
 
 } oam_entry;
 
+//typedef struct {
+//    u8 y;
+//    u8 x;
+//    u8 tile;
+//    u8 flags;
+//    unsigned f_cgb_pn : 3;
+//    unsigned f_cgb_vram_bank : 1;
+//    unsigned f_pn : 1;
+//    unsigned f_x_flip : 1;
+//    unsigned f_y_flip : 1;
+//    unsigned f_bgp : 1;
+//} oam_entry;
+
+typedef  struct _oam_line_entry {
+    oam_entry entry;
+    struct  _oam_line_entry *next;
+} oam_line_entry;
 /*
  Bit7   BG and Window over OBJ (0=No, 1=BG and Window colors 1-3 over the OBJ)
  Bit6   Y flip          (0=Normal, 1=Vertically mirrored)
@@ -63,28 +79,26 @@ typedef struct {
  Bit2-0 Palette number  **CGB Mode Only**     (OBP0-7)
  */
 
-typedef struct _oam_line_entry {
-    oam_entry entry;
-    struct _oam_line_entry *next;
-} oam_line_entry;
+
 
 typedef struct {
     oam_entry oam_ram[40];
     u8 vram[0x2000];
 
-    pixel_fifo_context pfc;
-
-    u8 line_sprite_count; //0 to 10 sprites.
-    oam_line_entry *line_sprites; //linked list of current sprites on line.
-    oam_line_entry line_entry_array[10]; //memory to use for list.
+    u8 line_sprite_count; // 0 to 10
+    oam_line_entry *line_sprites; //head of linked list
+    oam_line_entry line_entry_array[10];
 
     u8 fetched_entry_count;
-    oam_entry fetched_entries[3]; //entries fetched during pipeline.
+    oam_entry fetched_entries[3];
+
+    pixel_fifo_context pfc;
 
     u32 current_frame;
     u32 line_ticks;
     u32 *video_buffer;
 } ppu_context;
+
 
 void ppu_init();
 void ppu_tick();
